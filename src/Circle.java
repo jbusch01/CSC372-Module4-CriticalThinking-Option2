a421e4bcc37b9ed241cd03ac1ef08b9c4b74374c
jbusch01 <jonrbusch@gmail.com>
2025-05-11 09:15:25 -0600

    Updated Circle logic to use Math.PI directly

--- src/Circle.java
+++ src/Circle.java
@@ -1,21 +1,19 @@
 public class Circle extends Shape {
     private double radius;
-    private double pi = Math.PI;
 
     public Circle(double radius, double pi) {
         this.radius = radius;
-        this.pi = pi;
         
     }
 
     @Override
     public double area() {
-        return radius * radius * pi;
+        return radius * radius * Math.PI;
     }
     
     @Override
     public double perimeter() {
-        return 2 * pi * radius;
+        return 2 * Math.PI * radius;
     }
 
     @Override
